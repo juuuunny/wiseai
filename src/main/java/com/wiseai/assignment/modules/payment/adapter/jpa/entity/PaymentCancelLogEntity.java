777e1cc5df7f8dd042e8e55262b2dfd4b011b282
GitHub Action <action@github.com>
2025-11-19 16:59:18 +0000

    style: Spotless 자동 포맷팅 적용

--- src/main/java/com/wiseai/assignment/modules/payment/adapter/jpa/entity/PaymentCancelLogEntity.java
+++ src/main/java/com/wiseai/assignment/modules/payment/adapter/jpa/entity/PaymentCancelLogEntity.java
@@ -1,8 +1,8 @@
 package com.wiseai.assignment.modules.payment.adapter.jpa.entity;
 
-import com.wiseai.assignment.modules.payment.domain.enums.PaymentMethod;
-
 import java.time.LocalDateTime;
+
+import com.wiseai.assignment.modules.payment.domain.enums.PaymentMethod;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
