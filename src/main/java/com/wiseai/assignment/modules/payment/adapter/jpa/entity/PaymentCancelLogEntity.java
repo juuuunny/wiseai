f5cf18281bb55950142856dd4e5fd24471e64f00
juuuunny <102502542+juuuunny@users.noreply.github.com>
2025-11-20 01:58:47 +0900

    [fix] CI Spotless 규칙에 맞게 import 순서 수정 - com.wiseai를 java 앞으로 이동

--- src/main/java/com/wiseai/assignment/modules/payment/adapter/jpa/entity/PaymentCancelLogEntity.java
+++ src/main/java/com/wiseai/assignment/modules/payment/adapter/jpa/entity/PaymentCancelLogEntity.java
@@ -1,8 +1,8 @@
 package com.wiseai.assignment.modules.payment.adapter.jpa.entity;
 
-import java.time.LocalDateTime;
-
 import com.wiseai.assignment.modules.payment.domain.enums.PaymentMethod;
+
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
